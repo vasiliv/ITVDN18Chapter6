53603378b0229e91f2985f632aa1d1733d46777b
vasiliv <ggo24@yahoo.com>
2019-09-20 19:48:49 +0400

    Homework2and3

---
+++ Homework2and3/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Homework2and3.Models {
+    public class DatabaseContext : DbContext {
+        public DbSet <Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Homework2and3/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework2and3.Models {
+    public class Order {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        // ararsebulma klientma rom ar ganaxorcielos ararsebuli produqtis shekveta
+        public Product Product { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
