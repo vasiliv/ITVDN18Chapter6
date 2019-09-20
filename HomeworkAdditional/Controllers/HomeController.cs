e4ed7c22a16162758a7aa3035856234e83161dd5
vasiliv <ggo24@yahoo.com>
2019-09-20 11:30:48 +0400

    Add project files.

---
+++ HomeworkAdditional/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using HomeworkAdditional.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeworkAdditional.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(User user) {
+            Debug.WriteLine("Id = " + user.Id);
+            Debug.WriteLine("First Name = " + user.FirstName);
+            Debug.WriteLine("Last Name = " + user.LastName);
+            return View("Success");
+        }
+    }
+}
---
+++ HomeworkAdditional/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeworkAdditional.Models {
+    public class User {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
