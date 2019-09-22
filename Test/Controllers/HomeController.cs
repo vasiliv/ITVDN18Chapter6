e31c3903b242f6838b3b152586b9709b8a0ba68c
vasiliv <ggo24@yahoo.com>
2019-09-22 19:57:43 +0400

    Test

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers {
+    public class HomeController : Controller {
+        OnlineShopEntities db = new OnlineShopEntities();
+        public ActionResult Index() {
+            ViewBag.Products = new SelectList(db.Products,"Id","Name") ;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ReceiveData() {
+            ViewBag.ReceiveData = Products.
+            return View();
+        }
+    }
+}
